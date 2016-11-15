87ec72d938e4a987f3426a059f5a8a0a6d1bea23
Luyuan Xing <lxing.nyc@gmail.com>
2016-11-15 14:05:28 -0500

    migrated new column for patients table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115180138) do
+ActiveRecord::Schema.define(version: 20161115190145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20161115180138) do
     t.string   "name"
     t.string   "sickness"
     t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "secret_info"
   end
 
   create_table "users", force: :cascade do |t|
